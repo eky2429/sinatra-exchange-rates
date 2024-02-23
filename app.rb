bd18d351e5ee3994c29e887b691ab51eed04068a
Ethan Ky <132713901+eky2429@users.noreply.github.com>
2024-02-23 21:28:48 +0000

    My first 2 pages started working woohoo!

--- app.rb
+++ app.rb
@@ -6,12 +6,11 @@ get("/") do
   ctx = OpenSSL::SSL::SSLContext.new
   ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-
-  #api_url = "http://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATES_KEY")}"
-  api_url = "http://api.exchangerate.host/list?access_key=4eb345ecbefe16b7159612f18206159b"
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATES_KEY")}"
+  #api_url = "http://api.exchangerate.host/list?access_key=4eb345ecbefe16b7159612f18206159b"
 
   # use HTTP.get to retrieve the API information
-  raw_data = HTTP.get(api_url,  :ssl_context => ctx)
+  raw_data = HTTP.get(api_url)
   pp raw_data
 
   # convert the raw request to a string
@@ -20,24 +19,38 @@ get("/") do
   # convert the string to JSON
   parsed_data = JSON.parse(raw_data_string)
 
-  parsed_data
-
-  #   currencies = parsed_data.fetch("currencies")
-  #   other_elements = "<ul>"
-  #   currencies.each do
-  #     |key|
-  #     other_elements += "<li> Convert 1 #{key} to ...</li>"
-  #   end
-  #   other_elements += "</ul>"
-  # end
+  currencies = parsed_data.fetch("currencies")
+  other_elements = "<ul>"
+  currencies.each do
+    |key, _|
+    other_elements += "<li> <a href = \"#{key}\">Convert 1 #{key} to ... </a></li>"
+  end
+  other_elements += "</ul>"
 end
 
 get("/:from_currency") do
   @original_currency = params.fetch("from_currency")
 
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATES_KEY")}"
+  #api_url = "http://api.exchangerate.host/list?access_key=4eb345ecbefe16b7159612f18206159b"
 
-  # some more code to parse the URL and render a view template
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+  pp raw_data
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  currencies = parsed_data.fetch("currencies")
+  other_elements = "<ul>"
+  currencies.each do
+    |key, _|
+    other_elements += "<li> <a href = \"#{@original_currency}/#{key}\">Convert 1 #{@original_currency} to #{key}</a></li>"
+  end
+  other_elements += "</ul>"
 end
 
 get("/:from_currency/:to_currency") do
